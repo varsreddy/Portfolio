098b6bf3a72a7fb041fe95864b2e6a508e30876f
varsreddy <varshithak809@gmail.com>
2025-06-29 22:02:59 +0530

    Resume Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -55,7 +55,7 @@ const About = () => {
             {/* Buttons */}
             <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start animate-fade-in">
               <a
-                href="https://drive.google.com/file/d/1nRvgjGwgFes_KCzUCvR5vndlPzIF7cGs/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1gRhNxnKNre88arfoqHCbzO8DbpLjSvtW/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-white py-3 px-8 rounded-full font-bold text-lg shadow-md transition-transform transform hover:scale-105 bg-gradient-to-r from-[#8245ec] to-[#a855f7]"
